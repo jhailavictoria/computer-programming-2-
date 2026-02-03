309db1b96f293c0716692eef19d59ffa76fe4a91
jhailavictoria <lavictoria.jhai07@gmail.com>
2026-02-03 16:27:03 +0800

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,18 @@
+# Create variables to store personal information
+fullname = "Jhai M. La Victoria"
+age = 18
+height_in_meters = 1.70
+is_working = False
+
+# Display the values and their data types
+print(f"Fullname: {fullname}")
+print(type(fullname))
+
+print(f"Age: {age}")
+print(type(age))
+
+print(f"Height in meters: {height_in_meters}")
+print(type(height_in_meters))
+
+print(f"Currently working? {is_working}")
+print(type(is_working))
